8347206ad289058517b495402ccebc0a9a6d9c01
LaM0uette <LaM0uette.dev@gmail.com>
2022-10-12 10:42:56 +0200

    CheckPoolSize

--- FilesDirCmd/Core/Search.cs
+++ FilesDirCmd/Core/Search.cs
@@ -13,4 +13,22 @@ public static class Search
         
         Var.Log.Param("REQUETE UTILISEE : ", flags.GetReqOfSearch());
     }
+    
+    //
+
+    #region Fonctions
+
+    public static void CheckPoolSize(this Structs.SFlags flags)
+    {
+        if (flags.PoolSize < 2)
+        {
+            flags.PoolSize = 2;
+            Var.Log.Nok("Poolsize trop petite, impossible de mettre moins de 2");
+            Var.Log.Info("Modification de la Poolsize en cours");
+        }
+        
+        Var.Log.Param("POOLSIZE MISE A : ", flags.PoolSize.ToString());
+    }
+
+    #endregion
 }
--- FilesDirCmd/FilesDir.cs
+++ FilesDirCmd/FilesDir.cs
@@ -31,6 +31,7 @@ public static class FilesDir
         Drawing.Start();
         
         flags.Init();
+        flags.CheckPoolSize();
 
         //Drawing.End();
     }
