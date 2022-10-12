89c1adcf3bb08ede343aad59ec90e770349b869c
LaM0uette <LaM0uette.dev@gmail.com>
2022-10-12 15:03:08 +0200

    up

--- FilesDirCmd/Core/Search.cs
+++ FilesDirCmd/Core/Search.cs
@@ -5,48 +5,8 @@ using FilesDir.Utils;
 
 namespace FilesDirCmd.Core;
 
-public static class Search
+public static partial class Search
 {
-    #region Configurations
-
-    public static void Init(this Structs.SFlags flags)
-    {
-        Var.Log.Separator("PARAMETRES");
-        Var.Log.Param("INITIALISATION DE LA RECHERCHE EN COURS...");
-
-        Var.Log.Param("CREATION DE LA REQUETE EN COURS...");
-        Var.Results.Req = flags.GetReqOfSearch();
-        
-        Var.Log.Param("REQUETE UTILISEE : ", Var.Results.Req);
-        
-        Var.Log.Param("INITIALISATION DU FICHIER DUMP...");
-        Var.Dump.String("Id;Fichier;Date creation;Date de modification;Lien du fichier;Lien du dossier");
-    }
-
-    public static void CheckPoolSize(this Structs.SFlags flags)
-    {
-        if (flags.PoolSize < 2)
-        {
-            Var.Log.Nok("Poolsize trop petite, impossible de mettre moins de 2");
-            Var.Log.Param("Modification de la Poolsize en cours...");
-            flags.PoolSize = 2;
-        }
-        
-        Var.Log.Param("POOLSIZE MISE A : ", flags.PoolSize.ToString());
-    }
-
-    public static void SetPoolSize(this Structs.SFlags flags)
-    {
-        Var.ParallelOptions = new ParallelOptions {MaxDegreeOfParallelism = flags.PoolSize};
-        Var.Log.Param("CONFIGURATION DE LA POOLSIZE : ", "OK");
-    }
-
-    #endregion
-    
-    //
-    
-    #region Functions
-
     public static async Task Run(this Structs.SFlags flags)
     {
         var sw = new Stopwatch();
@@ -64,6 +24,4 @@ public static class Search
         // TODO: A SUPPRIMER ET A REMPLACER PAR UN BILAN
         Var.Log.VoidPink($"{Var.Results.NbFiles} - {Var.Results.SearchTimer} - {Var.Results.NbFilesTotal} - {Var.Results.NbFolders}");
     }
-
-    #endregion
 }
---
+++ FilesDirCmd/Core/SearchConfigs.cs
@@ -0,0 +1,39 @@
+﻿using FilesDir.Core;
+using FilesDir.Utils;
+
+namespace FilesDirCmd.Core;
+
+public static partial class Search
+{
+    public static void Init(this Structs.SFlags flags)
+    {
+        Var.Log.Separator("PARAMETRES");
+        Var.Log.Param("INITIALISATION DE LA RECHERCHE EN COURS...");
+
+        Var.Log.Param("CREATION DE LA REQUETE EN COURS...");
+        Var.Results.Req = flags.GetReqOfSearch();
+        
+        Var.Log.Param("REQUETE UTILISEE : ", Var.Results.Req);
+        
+        Var.Log.Param("INITIALISATION DU FICHIER DUMP...");
+        Var.Dump.String("Id;Fichier;Date creation;Date de modification;Lien du fichier;Lien du dossier");
+    }
+
+    public static void CheckPoolSize(this Structs.SFlags flags)
+    {
+        if (flags.PoolSize < 2)
+        {
+            Var.Log.Nok("Poolsize trop petite, impossible de mettre moins de 2");
+            Var.Log.Param("Modification de la Poolsize en cours...");
+            flags.PoolSize = 2;
+        }
+        
+        Var.Log.Param("POOLSIZE MISE A : ", flags.PoolSize.ToString());
+    }
+
+    public static void SetPoolSize(this Structs.SFlags flags)
+    {
+        Var.ParallelOptions = new ParallelOptions {MaxDegreeOfParallelism = flags.PoolSize};
+        Var.Log.Param("CONFIGURATION DE LA POOLSIZE : ", "OK");
+    }
+}
