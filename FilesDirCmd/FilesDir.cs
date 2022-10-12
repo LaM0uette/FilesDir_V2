3208d68f587d2e44aa6d3874f17eaac6263baae6
LaM0uette <LaM0uette.dev@gmail.com>
2022-10-12 09:13:54 +0200

    up

--- FilesDirCmd/FilesDir.cs
+++ FilesDirCmd/FilesDir.cs
@@ -10,15 +10,6 @@ public static class FilesDir
     {
         var flags = Api.GetFlags();
         
-        Console.WriteLine(flags.SearchMode);
-        foreach (var word in flags.Words) Console.WriteLine(word);
-        foreach (var ext in flags.Extensions) Console.WriteLine(ext);
-        Console.WriteLine(flags.PoolSize);
-        Console.WriteLine(flags.Casse);
-        Console.WriteLine(flags.Utf);
-        Console.WriteLine(flags.Silent);
-        foreach (var folder in flags.FoldersBlackList) Console.WriteLine(folder);
-        foreach (var folder in flags.FoldersWhiteList) Console.WriteLine(folder);
     }
 
     #endregion
