1acd2b29c8f68301251db6c793e6c1822252c5c3
LaM0uette <LaM0uette.dev@gmail.com>
2022-10-14 15:28:51 +0200

    param > drawParam

--- FilesDir/Flags/Config.cs
+++ FilesDir/Flags/Config.cs
@@ -1,4 +1,5 @@
 ﻿using FilesDir.Utils;
+using Logger;
 
 namespace FilesDir.Flags;
 
@@ -7,17 +8,17 @@ public partial class Flags
     private void Init()
     {
         Var.Log.Separator("PARAMETRES");
-        Var.Log.Param("INITIALISATION DE LA RECHERCHE EN COURS...");
+        Var.Log.DrawParam("INITIALISATION DE LA RECHERCHE EN COURS...");
 
-        Var.Log.Param("CREATION DE LA REQUETE EN COURS...");
+        Var.Log.DrawParam("CREATION DE LA REQUETE EN COURS...");
         Var.Results.Req = GetReqOfSearch();
         
-        Var.Log.Param("REQUETE UTILISEE : ", Var.Results.Req);
+        Var.Log.DrawParam("REQUETE UTILISEE : ", Var.Results.Req);
         
-        Var.Log.Param("INITIALISATION DU FICHIER DUMP...");
+        Var.Log.DrawParam("INITIALISATION DU FICHIER DUMP...");
         Var.Dump.String("Id;Fichier;Date creation;Date de modification;Lien du fichier;Lien du dossier");
         
-        Var.Log.Param("CREATION DU DOSSIER D'EXPORT...");
+        Var.Log.DrawParam("CREATION DU DOSSIER D'EXPORT...");
         Tasks.CreateDir(Path.Join(Var.DirTemp, "exports"));
     }
 
@@ -26,16 +27,16 @@ public partial class Flags
         if (PoolSize < 2)
         {
             Var.Log.Nok("Poolsize trop petite, impossible de mettre moins de 2");
-            Var.Log.Param("Modification de la Poolsize en cours...");
+            Var.Log.DrawParam("Modification de la Poolsize en cours...");
             PoolSize = 2;
         }
         
-        Var.Log.Param("POOLSIZE MISE A : ", PoolSize.ToString());
+        Var.Log.DrawParam("POOLSIZE MISE A : ", PoolSize.ToString());
     }
 
     private void SetPoolSize()
     {
         Var.ParallelOptions = new ParallelOptions {MaxDegreeOfParallelism = PoolSize};
-        Var.Log.Param("CONFIGURATION DE LA POOLSIZE : ", "OK");
+        Var.Log.DrawParam("CONFIGURATION DE LA POOLSIZE : ", "OK");
     }
 }
