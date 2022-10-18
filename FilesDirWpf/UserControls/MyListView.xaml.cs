8366ac106477bca2c96ebc20188608854c3e3683
LaM0uette <LaM0uette.dev@gmail.com>
2022-10-18 13:52:45 +0200

    add custom lists

---
+++ FilesDirWpf/UserControls/MyListView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Windows;
+
+namespace FilesDirWpf.UserControls;
+
+public partial class MyListView
+{
+    #region Statements
+
+    public MyListView()
+    {
+        InitializeComponent();
+    }
+
+    #endregion
+
+    //
+
+    #region Fonctions
+
+    private void Clear() => LstBx.Items.Clear();
+
+    private void SetListData(IEnumerable lst)
+    {
+        foreach (var str in lst)
+        {
+            LstBx.Items.Add(str);
+        }
+    }
+
+    #endregion
+    
+    //
+
+    #region Actions
+
+    private void BtnPlus_OnClick(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnMinus_OnClick(object sender, RoutedEventArgs e)
+    {
+        var selected = LstBx.SelectedItems.Cast<object>().ToArray();
+        foreach (var item in selected) LstBx.Items.Remove(item);
+    }
+
+    private void BtnEdit_OnClick(object sender, RoutedEventArgs e)
+    {
+        Clear();
+    }
+
+    private void BtnCleen_OnClick(object sender, RoutedEventArgs e)
+    {
+        Clear();
+    }
+
+    #endregion
+}
--- FilesDirWpf/Views/FiltersView.xaml.cs
+++ FilesDirWpf/Views/FiltersView.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace FilesDirWpf.Views;
+﻿using System.Windows;
+using System.Windows.Controls;
+using FilesDirWpf.UserControls;
+
+namespace FilesDirWpf.Views;
 
 public partial class FiltersView
 {
     #region Statements
+    
+    private MyListView _mlvWords = new();
+    private MyListView _mlvExtensions = new();
+    private MyListView _mlvBlackList = new();
+    private MyListView _mlvWhiteList = new();
 
     public FiltersView()
     {
         InitializeComponent();
+
+        CreateUiList();
     }
 
     #endregion
@@ -14,6 +25,23 @@ public partial class FiltersView
     //
 
     #region Fonctions
+    
+    private void CreateUiList()
+    {
+        void AddInGrid(UIElement lst, int row)
+        {
+            Grid.SetRow(lst, row);
+            Grid.SetColumn(lst, 2);
+            Grid.SetColumnSpan(lst, 3);
+            
+            GridLists.Children.Add(lst);
+        }
+
+        AddInGrid(_mlvWords, 1);
+        AddInGrid(_mlvExtensions, 3);
+        AddInGrid(_mlvBlackList, 5);
+        AddInGrid(_mlvWhiteList, 7);
+    }
 
     #endregion
 
